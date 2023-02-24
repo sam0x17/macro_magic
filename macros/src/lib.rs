6e0b5e4824a0ac28619980320c44df364d58c725
Sam Johnson <sam@durosoft.com>
2023-02-23 19:04:31 -0500

    export tokens working :boom:

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,24 @@
+extern crate proc_macro;
+use proc_macro::{Span, TokenStream};
+use quote::quote;
+use syn::{parse_macro_input, Error, Item};
 
+#[proc_macro_attribute]
+pub fn export_tokens(attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    if !attr.is_empty() {
+        return Error::new(
+            Span::call_site().into(),
+            "#[export_tokens] does not take any arguments",
+        )
+        .to_compile_error()
+        .into();
+    }
+    let tmp = tokens.clone();
+    let item: Item = parse_macro_input!(tmp as Item);
+    let source_code = tokens.to_string();
+    quote! {
+        #item
+        const SOURCE_CODE: &'static str = #source_code;
+    }
+    .into()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+pub use macros::export_tokens;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_export_tokens() {
+        #[export_tokens]
+        fn _add(a: usize, b: usize) -> usize {
+            a + b
+        }
+        assert_eq!(
+            SOURCE_CODE,
+            "fn _add(a : usize, b : usize) -> usize { a + b }"
+        );
+    }
+}
